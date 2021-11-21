1b27a7429c3009753b7b27ff37cf2cfc5906db68
NordJavaDev <nordjavadev@gmail.com>
2021-11-21 17:16:54 +0300

    added day 4

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,33 @@
 package day4;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        int[] numb = new int[100];
+        for(int i = 0; i < numb.length; i++){
+            Random random = new Random();
+            numb[i] = random.nextInt(10000);
+        }
+        int max = 0;
+        int min = 10000;
+        int wN = 0;
+        int sumN = 0;
+        for(Integer x : numb){
+            if(x > max){
+                max = x;
+            }
+            if(x<=min){
+                min = x;
+            }
+            if(x % 10 == 0){
+                wN++;
+                sumN +=x;
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(wN);
+        System.out.println(sumN);
     }
 }
--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -1,7 +1,30 @@
 package day4;
 
+import java.util.Random;
+
 public class Task3 {
     public static void main(String[] args) {
+        int[][] matrice = new int[12][8];
 
+        int sumInLine = 0;
+        int ind = 0;
+        for (int i = 0; i < matrice.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < matrice[i].length; j++) {
+                Random random = new Random();
+                matrice[i][j] = random.nextInt(50);
+                System.out.print(matrice[i][j] + " ");
+                sum += matrice[i][j];
+            }
+
+            System.out.println();
+
+            if(sum >= sumInLine){
+                sumInLine = sum;
+                ind = i;
+            }
+        }
+        System.out.println();
+        System.out.println(ind);
     }
 }
